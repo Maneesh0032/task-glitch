cbc980baf19270e2a44c493bc7e5c12ef6f52a9c
Maneesh0032 <126312620+Maneesh0032@users.noreply.github.com>
2026-01-16 03:40:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
-
+      "@": "/src"
+    }
+  }
+})
